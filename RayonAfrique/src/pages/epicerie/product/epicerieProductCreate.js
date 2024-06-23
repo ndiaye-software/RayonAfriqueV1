15d1dc2c3d653516ffa8760ceb59c507eee544e1
Mouhamadou-Ndiaye <mouhamadoundiaye1290@gmail.com>
2024-06-23 16:51:06 +0200

    hide the keys

--- RayonAfrique/src/pages/epicerie/product/epicerieProductCreate.js
+++ RayonAfrique/src/pages/epicerie/product/epicerieProductCreate.js
@@ -226,11 +226,11 @@ function EpicerieProductCreate() {
       // 1. Upload image to Cloudinary
       const formDataImage = new FormData();
       formDataImage.append("file", formData.image);
-      formDataImage.append("upload_preset", "rayonafrique");
+      formDataImage.append("upload_preset", process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
       formDataImage.append("public_id", uuidv4());
 
       const cloudinaryResponse = await fetch(
-        "https://api.cloudinary.com/v1_1/dpodybbfe/image/upload",
+        `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
         {
           method: "POST",
           body: formDataImage,
