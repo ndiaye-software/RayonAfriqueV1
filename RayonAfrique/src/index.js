149bf1e8c09be3fd4ce1fb2943c419647b965303
Mouhamadou-Ndiaye <mouhamadoundiaye1290@gmail.com>
2024-06-24 16:05:51 +0200

    petite correction

--- RayonAfrique/src/index.js
+++ RayonAfrique/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
-if(process.env.NODE_ENV == 'production') disableReactDevTools();
+if(process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
