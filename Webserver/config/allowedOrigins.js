a8761dd9aec594d1485e8d7ef60929c2dfa56e78
Mouhamadou-Ndiaye <mouhamadoundiaye1290@gmail.com>
2024-07-05 17:36:28 +0200

    remove index.html head

--- Webserver/config/allowedOrigins.js
+++ Webserver/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 const allowedOrigins = 
-[ "https://rayonafrique.onrender.com", "http://localhost:3000"];
+[ "https://rayonafrique.onrender.com", "http://localhost:3000", "https://rayonafrique.fr"];
 
 module.exports = allowedOrigins;
